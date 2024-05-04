a2cc6b957965c8b01dc6dac01615432471e8ef09
kasylozy <kasylozy@gmail.com>
2024-05-04 19:11:22 -0400

    Add alacritty

--- install.sh
+++ install.sh
@@ -3,51 +3,98 @@
 set -e
 set -x
 
-function install_packages ()
+function install_packages()
 {
-	sudo pacman -Syyu --needed --noconfirm \
-		fuse2 \
-		wine \
-		wine-mono \
-		lib32-vkd3d \
-		wine-gecko \
-		winetricks \
-		git \
-		rofi \
-		polybar \
-		rsync \
-		ttf-font-awesome \
-		awesome-terminal-fonts \
-		powerline \
-		powerline-fonts \
-		wqy-bitmapfont \
-		wqy-microhei \
-		wqy-microhei-lite \
-		wqy-zenhei \
-		ttf-font-awesome \
-		ttf-roboto \
-		ttf-roboto-mono \
-		noto-fonts-cjk \
-		adobe-source-han-serif-cn-fonts \
-		picom
+        sudo pacman -Syyu --needed --noconfirm \
+                wine \
+                firefox \
+                wget \
+                vim \
+                vi \
+                pwgen \
+                htop \
+                alacritty \
+                chromium \
+                vivaldi \
+                vivaldi-ffmpeg-codecs \
+                discord \
+                polkit-gnome \
+                libreoffice-fresh \
+                nm-connection-editor \
+                networkmanager \
+                networkmanager-openvpn \
+                ntfs-3g \
+                vlc \
+                gnome-system-monitor \
+                udisks2 \
+                remmina \
+                freerdp \
+                zip \
+                unzip \
+                mariadb \
+                mariadb-clients \
+                postfix \
+                npm \
+                ruby \
+                zsh \
+                wine-mono \
+                lib32-vkd3d \
+                wine-gecko \
+                winetricks \
+                git \
+                rofi \
+                polybar \
+                rsync \
+                ttf-font-awesome \
+                awesome-terminal-fonts \
+                powerline \
+                powerline-fonts \
+                wqy-bitmapfont \
+                wqy-microhei \
+                wqy-microhei-lite \
+                wqy-zenhei \
+                ttf-font-awesome \
+                ttf-roboto \
+                ttf-roboto-mono \
+                noto-fonts-cjk \
+                adobe-source-han-serif-cn-fonts \
+                picom \
+                feh \
+                lxappearance \
+                thunar \
+                thunar-volman \
+                xfce4-settings \
+                neofetch \
+                spotify-launcher \
+                bitwarden \
+                base-devel \
+                dkms \
+                arc-gtk-theme \
+                gnome-screenshot \
+                gnome-disk-utility \
+                nautilus \
+                docker
 }
 
-function configuration_yay
+function configuration_yay()
 {
-        if [ ! -d ./yay-bin ]; then
-                if [ ! -f /usr/bin/git ]; then
-                        sudo pacman -S --needed git base-devel
-                fi
+        if [ ! -f /usr/bin/yay ]; then
                 rm -Rf ./yay-bin/
                 git clone https://aur.archlinux.org/yay-bin.git
                 cd ./yay-bin/
                 makepkg -si --noconfirm
-		cd ../
-		rm -Rf ./yay-bin/
+                cd ../
+                rm -Rf ./yay-bin/
         fi
 }
 
-function configure_keyboard_french_canada() 
+function install_aur()
+{
+        yay -Syyu --needed --noconfirm \
+                aur/opera
+}
+
+function configure_keyboard_french_canada()
 {
         keyboard_file=/etc/X11/xorg.conf.d/00-keyboard.conf
         if ! grep "ca(fr)" $keyboard_file &>/dev/null; then
@@ -55,17 +102,84 @@ function configure_keyboard_french_canada()
         fi
 }
 
-function update_config() 
+function enable_network_manager()
 {
-	rsync -avPh ./config/* ~/.config/
+        if [ `systemctl is-enabled NetworkManager` = "disabled" ]; then
+                sudo systemctl enable --now NetworkManager
+        fi
 }
 
-function main ()
+function configure_mariadb()
 {
-	install_packages
-	configuration_yay
-	configure_keyboard_french_canada
-	update_config
+        if [ `systemctl is-enabled mariadb` = "disabled" ]; then
+                sudo systemctl enable --now mariadb
+        fi
+}
+
+function configure_ohMyZsh()
+{
+        if [ ! -d ~/.oh-my-zsh ]; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" <<EOF
+        exit
+EOF
+        chsh -s $(which zsh)
+        sudo pacman -S --noconfirm keychain
+        mkdir -p -m 700 ~/.ssh
+        git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+        sed -i "s/ZSH_THEME=\"robbyrussell\"/#ZSH_THEME=\"robbyrussell\"/" ~/.zshrc
+        cat >> ~/.zshrc <<EOF
+fpath+=$HOME/.zsh/pure
+autoload -U promptinit; promptinit
+prompt pure
+zmodload zsh/nearcolor
+zstyle :prompt:pure:path color '#FFFFFF'
+zstyle ':prompt:pure:prompt:*' color cyan
+zstyle :prompt:pure:git:stash show yes
+eval \$(keychain --eval --agents ssh --quick --quiet)
+export TERM=xterm-256color
+EOF
+        fi
+}
+
+function configure_postfix()
+{
+        if [ `systemctl is-enabled postfix` = "disabled" ]; then
+                postfix_file=/etc/postfix/main.cf
+                sudo chmod o+w $postfix_file
+                sudo sed -i 's/#relayhost = \[an\.ip\.add\.ress\]/relayhost = 127\.0\.0\.1:1025/' $postfix_file
+                sudo chmod o-w $postfix_file
+                sudo systemctl enable --now postfix
+        fi
+}
+
+function maildev_docker()
+{
+        if [ `systemctl is-enabled docker.service` = "disabled" ] ; then
+                sudo systemctl enable --now docker.service
+        fi
+        if ! sudo docker ps | grep mail; then
+                sudo docker run -d --restart unless-stopped -p 1080:1080 -p 1025:1025 dominikserafin/maildev:latest
+        fi
+}
+
+function update_config()
+{
+        rsync -avPh ./config/* ~/.config/
+        rsync -avPh ./.alacritty.toml ~/
+}
+
+function main()
+{
+        install_packages
+        configuration_yay
+        install_aur
+        configure_keyboard_french_canada
+        enable_network_manager
+        configure_mariadb
+        configure_ohMyZsh
+        configure_postfix
+        maildev_docker
+        update_config
 }
 
 main
